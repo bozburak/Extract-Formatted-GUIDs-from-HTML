10bb93bc5c15ab996b424f4beaae61dafc1d2df5
Burak BOZ <boz-burak@hotmail.com>
2024-10-30 11:07:14 +0300

    initial

---
+++ ExtractFormattedGuidFromHtml.ps1
@@ -0,0 +1,8 @@
+﻿# HTML dosyasını oku
+$htmlContent = Get-Content -Path "dosya_adı.html" -Raw
+
+# GUID'leri bulmak için regex kullan
+$guids = [regex]::Matches($htmlContent, 'reports/([a-f0-9-]+)\?experience') | ForEach-Object { $_.Groups[1].Value }
+
+# GUID'leri istenilen formatta birleştir ve panoya kopyala
+($guids | ForEach-Object { "`"$_`"" }) -join ",`n" | Set-Clipboard
